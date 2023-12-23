a638a50c4fb621f083372ef229c7eb5317c641ed
gerblesh <101901964+gerblesh@users.noreply.github.com>
2023-12-23 00:21:49 -0800

    pls help

--- src/include/vkapp.h
+++ src/include/vkapp.h
@@ -3,35 +3,98 @@
 #include <GLFW/glfw3.h>
 
 typedef struct {
-    uint width;
-    uint height;
+    uint32_t width;
+    uint32_t height;
+    char* title;
     GLFWwindow* window;
-} VKApp;
+    VkInstance instance;
+} VkApp;
 
-void app_initWindow(VKApp* app) {
+// Vulkan shtuff
+// TODO: reorg later :D
+
+void app_createVulkanInstance(VkApp* app) {
+    // initialize the extensions array
+    // app->extensionCount = 1;
+    // app->extensions = (VkExtensionProperties*)malloc(app->extensionCount * sizeof(VkExtensionProperties));
+
+    VkApplicationInfo appInfo;
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pNext = NULL;
+    appInfo.pApplicationName = app->title;
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+
+    VkInstanceCreateInfo createInfo;
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pNext = NULL;
+    createInfo.flags = VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
+    createInfo.pApplicationInfo = &appInfo;
+    createInfo.enabledLayerCount = 0;
+    createInfo.ppEnabledLayerNames = NULL;
+
+    // get the amount of instance extensions needed to allocate
+    uint32_t instnaceExtensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(NULL, &instnaceExtensionCount, NULL);
+    VkExtensionProperties* instanceExtensions = (VkExtensionProperties*)malloc(extensionCount * sizeof(VkExtensionProperties));
+    vkEnumerateInstanceExtensionProperties(NULL, &instnaceExtensionCount, instanceExtensions);
+    // get the glfw extensions:
+    uint32_t glfwExtensionCount = 0;
+    const char** glfwExtensions;
+    
+    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+    // get list of all extensions needed:
+    extensions
+    for (int i = 0; i<extensionCount + glfwExtensionCount) {
+        
+    }
+
+    createInfo.enabledExtensionCount = glfwExtensionCount;
+    createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+
+    // allocate for the function (for some reason thems don't allocate for ya)
+    // app->instance = (VkInstance*)malloc(sizeof(VkInstance));
+    printf("heyo, before the vkCreateInstance!\n");
+    VkResult result = vkCreateInstance(&createInfo, NULL, &app->instance);
+    printf("heyo, after the vkCreateInstance!\n");
+    free(extensions);
+    assert(result != VK_SUCCESS);
+}
+
+// GLFW shtuff
+
+void app_initVulkan(VkApp* app) {
+    app_createVulkanInstance(app);
+}
+
+void app_initWindow(VkApp* app) {
     glfwInit();
     
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
     glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-    app->window = glfwCreateWindow(app->width, app->height, "Vulkan", NULL, NULL);
+    app->window = glfwCreateWindow(app->width, app->height, app->title, NULL, NULL);
 }
 
-void app_initVulkan(VKApp* app) {
-    return;
-}
-
-void app_mainLoop(VKApp* app) {
+void app_mainLoop(VkApp* app) {
     while (!glfwWindowShouldClose(app->window)) {
         glfwPollEvents();
     }
 }
 
-void app_cleanup(VKApp* app) {
+void app_cleanup(VkApp* app) {
+    // delete vk instance I guess
+    vkDestroyInstance(app->instance, NULL);
+    // free array of extensions (malloced in app_createVulkanInstance)
+    
     glfwDestroyWindow(app->window);
     glfwTerminate();
 }
 
-int app_run(VKApp* app) {
+int app_run(VkApp* app) {
     app_initVulkan(app);
     app_mainLoop(app);
     app_cleanup(app);
--- src/main.c
+++ src/main.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <assert.h>
 #include "include/vkapp.h"
 
 
 
 int main() {
-    VKApp helloTriangle;
+    VkApp helloTriangle;
     helloTriangle.width = 600;
     helloTriangle.height = 800;
+    helloTriangle.title = "shartVK App";
     app_run(&helloTriangle);
     return 0;
 }
