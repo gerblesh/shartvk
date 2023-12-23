7fff46fb704635d29cb2df3c4e4fa3fb2ea6f195
gerblesh <101901964+gerblesh@users.noreply.github.com>
2023-12-23 10:36:38 -0800

    "fixed" vulkan for now??

--- src/include/vkapp.h
+++ src/include/vkapp.h
@@ -13,7 +13,7 @@ typedef struct {
 // Vulkan shtuff
 // TODO: reorg later :D
 
-void app_createVulkanInstance(VkApp* app) {
+void app_createVulkanInstance(VkApp* pApp) {
     // initialize the extensions array
     // app->extensionCount = 1;
     // app->extensions = (VkExtensionProperties*)malloc(app->extensionCount * sizeof(VkExtensionProperties));
@@ -21,84 +21,81 @@ void app_createVulkanInstance(VkApp* app) {
     VkApplicationInfo appInfo;
     appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
     appInfo.pNext = NULL;
-    appInfo.pApplicationName = app->title;
+    appInfo.pApplicationName = pApp->title;
     appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
     appInfo.pEngineName = "No Engine";
     appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
     appInfo.apiVersion = VK_API_VERSION_1_0;
 
-    VkInstanceCreateInfo createInfo;
-    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-    createInfo.pNext = NULL;
-    createInfo.flags = VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
-    createInfo.pApplicationInfo = &appInfo;
-    createInfo.enabledLayerCount = 0;
-    createInfo.ppEnabledLayerNames = NULL;
+    VkInstanceCreateInfo createInfo = {
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+        createInfo.flags = 0,
+        createInfo.pNext = NULL,
+        createInfo.pApplicationInfo = &appInfo,
+        createInfo.enabledLayerCount = 0,
+        createInfo.ppEnabledLayerNames = NULL
+    };
 
     // get the amount of instance extensions needed to allocate
-    uint32_t instnaceExtensionCount = 0;
-    vkEnumerateInstanceExtensionProperties(NULL, &instnaceExtensionCount, NULL);
-    VkExtensionProperties* instanceExtensions = (VkExtensionProperties*)malloc(extensionCount * sizeof(VkExtensionProperties));
-    vkEnumerateInstanceExtensionProperties(NULL, &instnaceExtensionCount, instanceExtensions);
+    // uint32_t instnaceExtensionCount = 0;
+    // vkEnumerateInstanceExtensionProperties(NULL, &instnaceExtensionCount, NULL);
     // get the glfw extensions:
     uint32_t glfwExtensionCount = 0;
     const char** glfwExtensions;
+
     
     glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
-
+    printf("glfw extensions: %d \n", glfwExtensionCount);
     // get list of all extensions needed:
-    extensions
-    for (int i = 0; i<extensionCount + glfwExtensionCount) {
-        
-    }
+
+    
 
     createInfo.enabledExtensionCount = glfwExtensionCount;
     createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+    // get supported extensions
+    // VkExtensionProperties* instanceExtensions = (VkExtensionProperties*)malloc(extensionCount * sizeof(VkExtensionProperties));
+    // vkEnumerateInstanceExtensionProperties(NULL, &instnaceExtensionCount, instanceExtensions);
 
 
     // allocate for the function (for some reason thems don't allocate for ya)
     // app->instance = (VkInstance*)malloc(sizeof(VkInstance));
     printf("heyo, before the vkCreateInstance!\n");
-    VkResult result = vkCreateInstance(&createInfo, NULL, &app->instance);
+    VkResult result = vkCreateInstance(&createInfo, NULL, &pApp->instance);
     printf("heyo, after the vkCreateInstance!\n");
-    free(extensions);
-    assert(result != VK_SUCCESS);
+    // free(extensions);
+    assert(result == VK_SUCCESS);
 }
 
 // GLFW shtuff
 
-void app_initVulkan(VkApp* app) {
-    app_createVulkanInstance(app);
+void app_initVulkan(VkApp* pApp) {
+    app_createVulkanInstance(pApp);
 }
 
-void app_initWindow(VkApp* app) {
+void app_initWindow(VkApp* pApp) {
     glfwInit();
     
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
     glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-    app->window = glfwCreateWindow(app->width, app->height, app->title, NULL, NULL);
+    pApp->window = glfwCreateWindow(pApp->width, pApp->height, pApp->title, NULL, NULL);
 }
 
-void app_mainLoop(VkApp* app) {
-    while (!glfwWindowShouldClose(app->window)) {
+void app_mainLoop(VkApp* pApp) {
+    while (!glfwWindowShouldClose(pApp->window)) {
         glfwPollEvents();
     }
 }
 
-void app_cleanup(VkApp* app) {
-    // delete vk instance I guess
-    vkDestroyInstance(app->instance, NULL);
-    // free array of extensions (malloced in app_createVulkanInstance)
-    
-    glfwDestroyWindow(app->window);
+void app_cleanup(VkApp* pApp) {
+    vkDestroyInstance(pApp->instance, NULL);
+    glfwDestroyWindow(pApp->window);
     glfwTerminate();
 }
 
-int app_run(VkApp* app) {
-    app_initVulkan(app);
-    app_mainLoop(app);
-    app_cleanup(app);
+int app_run(VkApp* pApp) {
+    app_initVulkan(pApp);
+    app_mainLoop(pApp);
+    app_cleanup(pApp);
     return 0;
 }
-
-
